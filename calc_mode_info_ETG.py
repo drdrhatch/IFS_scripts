933a5874f22aea3d9aefe3f1b7f0f0b78ed372b7
davidhat <drhatch@cori08.nersc.gov>
2019-08-12 10:00:04 -0700

    Scripte for calculating ETG mode info in linear scan.

---
+++ calc_mode_info_ETG.py
@@ -0,0 +1,31 @@
+from subprocess import call
+import os
+
+for i in range(100):
+    num = '0000'+str(i)
+    num = num[-4:]
+    print num
+    if os.path.exists('./parameters_'+num):
+        call(['calc_kpar_kperp_omd.py',num])
+
+files = []
+
+base = 'mode_info_1_'
+cat_string = 'cat '
+for i in range(50):  
+        suffix = '0000'+str(i)
+        suffix = suffix[-4:]
+        efile = base+suffix
+        if os.path.exists('./'+efile):
+            print efile
+            files.append(efile)
+            cat_string += efile+' '
+            print efile
+
+cat_string += '> '+'mode_info_all'
+
+call(cat_string,shell=True)
+
+        
+
+
