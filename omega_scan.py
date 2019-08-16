eaef8baa36d0a309f2277af97aa9ac64c719f389
drdrhatch <drdrhatch@gmail.com>
2019-08-16 23:01:27 +0200

    Added options for other algorithms for calculating omega

--- omega_scan.py
+++ omega_scan.py
@@ -3,13 +3,30 @@
 
 from omega_tool import omega_calc
 import os
+import optparse as op
+
+parser=op.OptionParser(description='Calculates growth rate and frequncy from linear scan.')
+parser.add_option('--alg','-a',action='store',type = int,help = 'Choose 1 for eigenmode average method and 2 for maximum value.',default = 1)
+parser.add_option('--single','-s',action='store',type = int,help = 'Analyze only one run in the scan (e.g. 0001).',default = 0)
+options,args=parser.parse_args()
+
+alg = options.alg
+single = options.single
+print "alg",alg
+print "single",single
 
 cwd = os.getcwd()
 filelist = []
-for filename in os.listdir(cwd):
-    if filename.startswith("omega"):
-        filelist.append(filename[-5:])
-filelist.sort()
+if float(single) != 0.0:
+   suffix = '0000'+str(single)
+   suffix = suffix[-4:]
+   filelist.append('_'+suffix)
+else:
+    for filename in os.listdir(cwd):
+        if filename.startswith("omega_0"):
+            filelist.append(filename[-5:])
+    filelist.sort()
 
 for suffix in filelist:
-    omega_calc(suffix)
+    print "suffix",suffix
+    omega_calc(suffix,alg = alg)
