6655d96efab77bb4324f4567f9dff929372ee911
drdrhatch <drdrhatch@gmail.com>
2020-09-17 20:18:15 -0700

    Added linear interpolation options

--- interp.py
+++ interp.py
@@ -1,5 +1,6 @@
 from scipy import interpolate
 import numpy as np
+import matplotlib.pyplot as plt
 
 def interp(xin,yin,xnew):
     """
@@ -24,8 +25,22 @@ def interp_lin(xin,yin,xnew):
     yout: new y interpolated on xnew
     """
 
+    if xnew[0] < xin[0]:
+       low_index = np.argmin(abs(xnew-xin[0]))
+       if xnew[low_index] < xin[0]:
+           low_index += 1
+    else:
+       low_index = 0
+    if xnew[-1] > xin[-1]:
+       high_index = np.argmin(abs(xnew-xin[-1]))
+       if xnew[high_index] > xin[0]:
+           high_index -= 1
+    else:
+        high_index = -1
+
     ynew = interpolate.interp1d(xin,yin)
-    yout = ynew(xnew)
+    yout = np.zeros(len(xnew))
+    yout[low_index:high_index] = ynew(xnew[low_index:high_index])
 
     return yout
 
@@ -69,21 +84,6 @@ def full_interp_lin(func_xin,xin,xconv,yconv,yout):
     yconv: ygrid for conversion
     yout: output grid
     """
-
-    #If necessary truncate func_xin onto correct range
-    if xin[0] < xconv[0]:
-        low_index = np.argmin(abs(xconv-xin[0]))
-    else:
-        low_index = 0
-    if xin[-1] > xconv[-1]:
-        high_index = np.argmin(abs(xconv-xin[-1]))
-    else:
-        high_index = -1
-
-    if high_index == -1:
-        func_xin = func_xin[low_index:]
-    else:
-        func_xin = func_xin[low_index:high_index]
 
     func_xconv = interp_lin(xin,func_xin,xconv)
     func_yout = interp_lin(yconv,func_xconv,yout)
