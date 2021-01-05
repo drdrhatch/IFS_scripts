922d727ada0004f75c8ca03074fcfd65f225075f
Justin Walker <walker.justinw@protonmail.com>
2021-01-05 10:20:23 -0600

    moved print statements

--- ballooning_structure.py
+++ ballooning_structure.py
@@ -121,10 +121,10 @@ if pods:
         if args.avgs:
             t, t_corr, corr_time = bl.autocorrelate(mode, u, times, axis=0)
             r, r_corr, corr_len = bl.autocorrelate(mode, VH["phi"], mode.zgrid_ext*np.pi, axis=-1)
-            print("corr_time = ", corr_time)
-            print("corr_len = ", corr_len)
             avg_freq = bl.avg_freq(times, u)
             avg_kz = bl.avg_kz(mode, VH["phi"])
+            print("corr_time = ", corr_time)
+            print("corr_len = ", corr_len)
             print("avg_freq = ", avg_freq)
             print("avg_kz = ", avg_kz)
             bl.output_scales(mode, avg_freq, "avg_freq")
