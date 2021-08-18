710fb3f057a6a70d33cdb7ce40d614b4587dedf9
Justin Walker <justin.walker@austin.utexas.edu>
2021-08-18 12:27:50 -0500

    separated z/t correlation and avg kz/om in POD section

--- ballooning_structure.py
+++ ballooning_structure.py
@@ -136,7 +136,7 @@ for i, mode in enumerate(ky_modes):
                 bl.plot_pod(mode, Q_pod, pods, "q", extend=False)
             for var in fields:
                 bl.plot_pod(mode, VH[var], pods, var)
-        if args.avgs:
+        if args.corr:
             t, t_corr, corr_time = bl.autocorrelate(mode, u, times, axis=0)
             r, r_corr, corr_len = bl.autocorrelate(
                 mode,
@@ -145,6 +145,9 @@ for i, mode in enumerate(ky_modes):
                 weights=mode.geometry["gjacobian"],
                 axis=-1,
             )
+            bl.output_scales(mode, corr_time, "corr_time")
+            bl.output_scales(mode, corr_len, "corr_len")
+        if args.avgs:
             if args.avgs == 1:
                 avg_freq = bl.avg_freq(times, u)
                 avg_kz = bl.avg_kz(mode, VH["phi"])
@@ -157,8 +160,6 @@ for i, mode in enumerate(ky_modes):
             )
             bl.output_scales(mode, avg_freq, "avg_freq")
             bl.output_scales(mode, avg_kz, "avg_kz")
-            bl.output_scales(mode, corr_time, "corr_time")
-            bl.output_scales(mode, corr_len, "corr_len")
     else:
         phi = mode.fields["phi"]
         scale_list = []
