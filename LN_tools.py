300dd6e2df2137de7a054a94cf4217728e9e8423
maxcurie1996 <maxcurie@utexas.edu>
2021-09-08 09:34:50 -0500

    Added progress bar

--- LN_tools.py
+++ LN_tools.py
@@ -7,6 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import imageio   #for making animation
 import csv
+from tqdm import tqdm
 
 from nrgWrapper import read_from_nrg_files
 from parIOWrapper import read_species_gradients
@@ -192,7 +193,7 @@ def k_f_plot(f_ky_f,amplitude_ky_f,ky_list,n_list,pic_path,csv_path,name='0'):
             csvfile.close()
 
     uni_freq = np.linspace(np.min(f_ky_f), np.max(f_ky_f),num=len(f_ky_f[0,:])*10)
-    print('len of uni_freq='+str(int(len(f_ky_f[0,:])*10)))
+    #print('len of uni_freq='+str(int(len(f_ky_f[0,:])*10)))
     len_uni_freq=len(uni_freq)
     frequency_kHZ_uni=np.zeros((nky0,len_uni_freq))
     amplitude_frequency_uni=np.zeros((nky0,len_uni_freq))
@@ -283,8 +284,8 @@ def k_f_density_plot(f_ky_f,amplitude_ky_f,ky_list,n_list,pic_path,csv_path,name
                 csv_data.writerow([ky_list[i],f_ky_f[i,j],amplitude_ky_f[i,j]])
             csvfile.close()
 
-    uni_freq = np.linspace(np.min(f_ky_f), np.max(f_ky_f),num=len(f_ky_f[0,:])*10)
-    print('len of uni_freq='+str(int(len(f_ky_f[0,:])*10)))
+    uni_freq = np.linspace(np.min(f_ky_f), np.max(f_ky_f),num=len(f_ky_f[0,:])*3)
+    #print('len of uni_freq='+str(int(len(f_ky_f[0,:])*3)))
     len_uni_freq=len(uni_freq)
     frequency_kHZ_uni=np.zeros((nky0,len_uni_freq))
     amplitude_frequency_uni=np.zeros((nky0,len_uni_freq))
@@ -295,7 +296,8 @@ def k_f_density_plot(f_ky_f,amplitude_ky_f,ky_list,n_list,pic_path,csv_path,name
     #amplitude_frequency_uni=np.zeros((nky0,len_uni_freq))
 
     ky_plot=np.zeros((nky0,len_uni_freq))
-    for i_ky in range(nky0):
+    print('Interpolate the with all ky')
+    for i_ky in tqdm(range(nky0)):
         frequency_kHZ_uni[i_ky,:]=uni_freq
         amplitude_frequency_uni[i_ky,:]=np.interp(uni_freq,f_ky_f[i_ky,:],amplitude_ky_f[i_ky,:])
         #for testing, not interprolation
@@ -436,8 +438,8 @@ def BES_f_spectrum_FFT(suffix,iterdb_file_name,manual_Doppler,min_Z0,max_Z0,\
     #Determine the n for kymin
     ky_n1=1.*q0*rhoref/(Lref*x0)  #ky for n=1
     n_list,ky_list=ky_list_calc(suffix)
-    print('n0 list length: '+str(len(n_list)))
-    print('n0 list: '+str(n_list))
+    #print('n0 list length: '+str(len(n_list)))
+    #print('n0 list: '+str(n_list))
     n_min=np.min(n_list)
     #ky_GENE_n1=ky_GENE_temp/float(n_min)
 
@@ -509,7 +511,7 @@ def BES_f_spectrum_FFT(suffix,iterdb_file_name,manual_Doppler,min_Z0,max_Z0,\
             n1=deln_global[:,:,:]+0.j
             n1_z_ky_kx=n1
             n1_GENE_ky0=n1_z_ky_kx*rhorefStar*nref
-            n1_GENE_ky= np.zeros(np.shape(n1_GENE_ky0[0,:,:]))
+            n1_GENE_ky= np.zeros(np.shape(n1_GENE_ky0[0,:,:]),dtype=complex)
 
             #*****Sum over Z************
             sum_length_TEMP=0
@@ -715,7 +717,7 @@ def BES_f_spectrum_density(suffix,iterdb_file_name,manual_Doppler,min_Z0,max_Z0,
             n1=deln_global[:,:,:]+0.j
             n1_z_ky_kx=n1
             n1_GENE_ky0=n1_z_ky_kx*rhorefStar*nref
-            n1_GENE_ky= np.zeros(np.shape(n1_GENE_ky0[0,:,:]))
+            n1_GENE_ky= np.zeros(np.shape(n1_GENE_ky0[0,:,:]),dtype=complex)
 
             #*****Sum over Z************
             sum_length_TEMP=0
@@ -924,7 +926,7 @@ def RIP_f_spectrum_FFT(suffix,iterdb_file_name,manual_Doppler,min_Z0,max_Z0,\
             apar=field.apar()[:,:,:]+0.j
             apar_z_ky_kx=apar
             B1_GENE_ky0=apar_z_ky_kx*(ky_GENE_grid*Apar_to_B1)
-            B1_GENE_ky= np.zeros(np.shape(B1_GENE_ky0[0,:,:]))
+            B1_GENE_ky= np.zeros(np.shape(B1_GENE_ky0[0,:,:]),dtype=complex)
 
             #*****Sum over Z************
             sum_length_TEMP=0
@@ -1052,8 +1054,8 @@ def RIP_f_spectrum_density(suffix,iterdb_file_name,manual_Doppler,min_Z0,max_Z0,
     #Determine the n for kymin
     ky_n1=1.*q0*rhoref/(Lref*x0)  #ky for n=1
     n_list,ky_list=ky_list_calc(suffix)
-    print('n0 list length: '+str(len(n_list)))
-    print('n0 list: '+str(n_list))
+    #print('n0 list length: '+str(len(n_list)))
+    #print('n0 list: '+str(n_list))
     n_min=np.min(n_list)
     #ky_GENE_n1=ky_GENE_temp/float(n_min)
 
@@ -1063,13 +1065,207 @@ def RIP_f_spectrum_density(suffix,iterdb_file_name,manual_Doppler,min_Z0,max_Z0,
         for j in range(nky0):
             for k in range(nkx0):
                 ky_GENE_grid[i,j,k]=ky_list[j]
+
+
+    #B1=abs(np.mean(Apar_GENE[z,:])*len(Apar_GENE[z,:])*(ky_GENE_temp[z]/rhoref)*Bref*B_gauss*rhorefStar*rhoref)
+    Apar_to_B1=abs((1./rhoref)*Bref*B_gauss*rhorefStar*rhoref)         #B1=Apar*ky_GENE_temp*Apar_to_B1
+
     
-    print("kygrid"+str(np.shape(ky_GENE_grid)))
+    time_start_index=np.argmin(abs(time - time_start))
+    time_end_index=np.argmin(abs(time - time_end))
+
+    time_list_temp = time[time_start_index:time_end_index+1]
+    time_list=[]
+
+    for i in range(0,len(time_list_temp),time_step):
+        time_list.append(time_list_temp[i])
+    time_list=np.array(time_list)
     
-    print('n0 list length: '+str(len(n_list)))
-    print('n0 list: '+str(n_list))
+    nky0=len(n_list)
+    ntime=len(time_list)
+
+    B1_ky_t=np.zeros((nky0,ntime),dtype=complex)
+
+    if os.path.isdir(csv_path):  #if path does not exist, then create 'csv'
+        pass
+    else:
+        os.mkdir(csv_path) 
+    if os.path.isdir(pic_path):  #if path does not exist, then create 'pic'
+        pass
+    else:
+        os.mkdir(pic_path) 
+    print("**********Scan starts, output in csv and pic***************")
+    
+    print('Loadind data across the time steps')
+    for i in tqdm(range(len(time_list))):
+        time0=time_list[i]
+        itime = np.argmin(abs(time - time0))
+        field.set_time(time_list[i])
+        #print("Looking at the spectra at time:"+str(time[itime]))
+        #This sets the time step you want to read in
+        #field.set_time(time[itime])
+        
+        if 'x_local' in pars:
+            if pars['x_local']:
+                x_local = True
+            else:
+                x_local = False 
+        else:
+            x_local = True
+
+        if x_local:
+            kxmin = 2.0*np.pi/pars['lx']
+            kxgrid = np.linspace(-(pars['nx0']/2-1)*kxmin,pars['nx0']/2*kxmin,num=pars['nx0'])
+            kxgrid = np.roll(kxgrid,int(pars['nx0']/2+1))
+            #print("kxgrid"+str(kxgrid))
+            
+            #print("kygrid"+str(kygrid))
+            zgrid = np.linspace(-np.pi,np.pi,pars['nz0'],endpoint=False)  
+
+            apar=field.apar()[:,:,:]+0.j
+            apar_z_ky_kx=apar
+            B1_GENE_ky0=apar_z_ky_kx*(ky_GENE_grid*Apar_to_B1)
+            B1_GENE_ky= np.zeros(np.shape(B1_GENE_ky0[0,:,0]),dtype=complex)
+
+            #*****Sum over Z************
+            sum_length_TEMP=0
+            if Outboard_mid_plane==True:
+                #nZ_list=[0,int(len(real_Z)/2)]
+                nZ_list=[int(len(real_Z)/2)]
+                for nZ in nZ_list:
+                    length=J[nZ]
+                    sum_length_TEMP=sum_length_TEMP+length
+                    B1_GENE_ky=B1_GENE_ky+np.sum(B1_GENE_ky0[nZ,:,:],axis=1)*length
+            else:
+                for nZ in range(len(real_Z)):
+                    if min_Z0<=real_Z[nZ] and real_Z[nZ]<=max_Z0:
+                        #length=np.sqrt( (real_R[nZ]-real_R[nZ-1])**2.\
+                        #    +(real_Z[nZ]-real_Z[nZ-1])**2. )
+                        #length=np.sqrt((real_Z[nZ]-real_Z[nZ-1])**2. )
+                        length=J[nZ]
+                        sum_length_TEMP=sum_length_TEMP+length
+                        B1_GENE_ky=B1_GENE_ky+np.sum(B1_GENE_ky0[nZ,:,:],axis=1)*length
+            B1_GENE_ky=B1_GENE_ky/sum_length_TEMP
+            #*****Sum over Z************
+            B1_ky=B1_GENE_ky
+
+        else:  #x_local = False
+            print("Sorry, cannot handle Global Nonlinear yet...")
+            pass
+        #**Finished reading the Br
+        #Recall B1_ky_t_inz=np.zeros((nky0,ntime))
+        B1_ky_t[:,i]=B1_ky
+    
+    ky_GENE_inz = ky_GENE_grid[int(len(real_Z)/2),:]
+    
+    amplitude_frequency_sum=0
+    amplitude_growth_sum=0
+    #print(str(B1_ky_t_inz))
+    if plot==True:
+        ims_n1=[]
+
+    B1_ky_f=[]
+    growth_ky_f=[]
+    f_ky_f=[]
+    time_list=time_list/gyroFreq*(1000.)
+
+    print('FFT across nky')
+
+    for iky in tqdm(range(nky0)):
+        B1_inz_t=(B1_ky_t[iky,:])
+        frequency,amplitude_frequency_sq=spectral_density(B1_inz_t,time_list,percent=percent_window,window_for_FFT=window_for_FFT,plot=False)
+        
+        amplitude_frequency=abs(np.sqrt(amplitude_frequency_sq))
+        if manual_Doppler==999:
+            omegaDoppler_kHZ=Doppler_calc(suffix,iky,iterdb_file_name)
+        else:
+            omegaDoppler_kHZ=manual_Doppler*n_list[iky]
+        frequency_kHZ=frequency+omegaDoppler_kHZ
+        if iky ==0:
+            new_frequency_kHZ_ky=frequency_kHZ
+            new_amplitude_frequency=(2.*amplitude_frequency)**2.
+        else:
+            new_amplitude_frequency=new_amplitude_frequency+(2.*amplitude_frequency)**2.
+            
+        f_ky_f.append(new_frequency_kHZ_ky)
+        B1_ky_f.append(new_amplitude_frequency**0.5)
     
     
+    f_ky_f=np.array(f_ky_f)
+    B1_ky_f=abs(np.array(B1_ky_f))
+
+    f,amplitude_f=k_f_density_plot(f_ky_f,B1_ky_f,ky_list,n_list,pic_path,csv_path,name='B1')
+    
+    return f,amplitude_f
+
+
+def RIP_f_spectrum_density_old(suffix,iterdb_file_name,manual_Doppler,min_Z0,max_Z0,\
+    Outboard_mid_plane,time_step,time_start,time_end,percent_window,window_for_FFT,\
+    plot,show,csv_output,pic_path,csv_path):
+    #Where inz is the number of the element in nz
+
+    #Import the parameters from parameter file using ParIO
+    par = Parameters()
+    par.Read_Pars('parameters'+suffix)
+    pars = par.pardict
+    #getting B field using read_write_geometry.py
+    gpars,geometry = read_geometry_local(pars['magn_geometry'][1:-1]+suffix)
+    #Get geom_coeff from ParIO Wrapper
+    geom_type, geom_pars, geom_coeff = init_read_geometry_file(suffix, pars)
+    real_Z=geometry['gl_z'] 
+    real_R=geometry['gl_R'] 
+    #Import the field file using fieldlib
+    field = fieldfile('field'+suffix,pars)
+    time = np.array(field.tfld)  #time stampes
+    J=geometry['gjacobian']
+
+    B_gauss=10.**4.              #1T=10^4Gauss
+    qref = 1.6E-19              #in C
+    c  = 1.                     #in 3*10^8m/s
+    m_kg = 1.673E-27            #in kg
+    Bref = pars['Bref']         #in Tesla
+    Tref = pars['Tref']         #in keV
+    nref = pars['nref']         #in 10^(19) /m^3
+    Lref = pars['Lref']         #in m
+    mref = pars['mref']         #in proton mass(kg)
+    q0 = pars['q0']              #unitless, safety factor/q95
+    x0 = pars['x0']             #x/a, location
+    kymin = pars['kymin']       #in rhoi
+    nky0 = pars['nky0']         #in total number of ky
+    nkx0 = pars['nx0']         #in total number of ky
+    n_step = pars['n0_global']  #in rhoi
+    nref = nref * 1.E19         #in the unit of /m^3
+    Tref = Tref * qref * 1.E03  #in the unit of J
+    mref = mref * m_kg          #in the unit of kg
+    pref = nref * Tref          #in Pa*k_{B}
+    cref = np.sqrt(Tref / mref) #in the unit of m/s
+    Omegaref = qref * Bref / mref / c  #in rad/s
+    rhoref = cref / Omegaref           #in m rho_i(ion gyroradii)
+    print('rhoref='+str(rhoref))
+    rhorefStar = rhoref / Lref         #Unitless
+    gyroFreq= cref/Lref                 #the facor convert frequency from cs/a to rad/s
+
+
+    #ky comes from geomWrapper.py
+    ky_GENE_temp=ky(pars, geom_coeff,plot=False)     #ky for ky min for differen z
+    #print('ky shape: '+str(np.shape(ky_GENE_temp)))
+
+    #Determine the n for kymin
+    ky_n1=1.*q0*rhoref/(Lref*x0)  #ky for n=1
+    n_list,ky_list=ky_list_calc(suffix)
+    #print('n0 list length: '+str(len(n_list)))
+    #print('n0 list: '+str(n_list))
+    n_min=np.min(n_list)
+    #ky_GENE_n1=ky_GENE_temp/float(n_min)
+
+    nz=len(real_Z)
+    ky_GENE_grid=np.zeros((nz,nky0,nkx0))#outer product of the two vectors
+    for i in range(nz):
+        for j in range(nky0):
+            for k in range(nkx0):
+                ky_GENE_grid[i,j,k]=ky_list[j]
+
+
     #B1=abs(np.mean(Apar_GENE[z,:])*len(Apar_GENE[z,:])*(ky_GENE_temp[z]/rhoref)*Bref*B_gauss*rhorefStar*rhoref)
     Apar_to_B1=abs((1./rhoref)*Bref*B_gauss*rhorefStar*rhoref)         #B1=Apar*ky_GENE_temp*Apar_to_B1
 
@@ -1098,12 +1294,13 @@ def RIP_f_spectrum_density(suffix,iterdb_file_name,manual_Doppler,min_Z0,max_Z0,
     else:
         os.mkdir(pic_path) 
     print("**********Scan starts, output in csv and pic***************")
-
-    for i in range(len(time_list)):
+    
+    print('Loadind data across the time steps')
+    for i in tqdm(range(len(time_list))):
         time0=time_list[i]
         itime = np.argmin(abs(time - time0))
         field.set_time(time_list[i])
-        print("Looking at the spectra at time:"+str(time[itime]))
+        #print("Looking at the spectra at time:"+str(time[itime]))
         #This sets the time step you want to read in
         #field.set_time(time[itime])
         
@@ -1127,7 +1324,7 @@ def RIP_f_spectrum_density(suffix,iterdb_file_name,manual_Doppler,min_Z0,max_Z0,
             apar=field.apar()[:,:,:]+0.j
             apar_z_ky_kx=apar
             B1_GENE_ky0=apar_z_ky_kx*(ky_GENE_grid*Apar_to_B1)
-            B1_GENE_ky= np.zeros(np.shape(B1_GENE_ky0[0,:,:]))
+            B1_GENE_ky= np.zeros(np.shape(B1_GENE_ky0[0,:,:]),dtype=complex)
 
             #*****Sum over Z************
             sum_length_TEMP=0
@@ -1151,10 +1348,6 @@ def RIP_f_spectrum_density(suffix,iterdb_file_name,manual_Doppler,min_Z0,max_Z0,
             #*****Sum over Z************
 
             B1_ky_kx=B1_GENE_ky
-            print('*****************')
-            print('*****B1_ky*******')
-            print('*****************')
-            print(B1_ky_kx)
 
         else:  #x_local = False
             print("Sorry, cannot handle Global Nonlinear yet...")
@@ -1175,7 +1368,10 @@ def RIP_f_spectrum_density(suffix,iterdb_file_name,manual_Doppler,min_Z0,max_Z0,
     growth_ky_f=[]
     f_ky_f=[]
     time_list=time_list/gyroFreq*(1000.)
-    for iky in range(nky0):
+
+    print('FFT across nky')
+
+    for iky in tqdm(range(nky0)):
         count_TEMP=0
         for ikx in range(nkx0):
             B1_inz_t=(B1_ky_kx_t[iky,ikx,:])
@@ -1266,14 +1462,14 @@ def RIP_k_space_sum_IDL(suffix,iterdb_file_name,manual_Doppler,\
     #Determine the n for kymin
     ky_n1=1.*q0*rhoref/(Lref*x0)  #ky for n=1
     n_list,ky_list=ky_list_calc(suffix)
-    print('n0 list length: '+str(len(n_list)))
-    print('n0 list: '+str(n_list))
+    #print('n0 list length: '+str(len(n_list)))
+    #print('n0 list: '+str(n_list))
     n_min=np.min(n_list)
     
     #print("kygrid"+str(np.shape(ky_GENE_grid)))
     
-    print('n0 list length: '+str(len(n_list)))
-    print('n0 list: '+str(n_list))
+    #print('n0 list length: '+str(len(n_list)))
+    #print('n0 list: '+str(n_list))
     
     
     #B1=abs(np.mean(Apar_GENE[z,:])*len(Apar_GENE[z,:])*(ky_GENE_temp[z]/rhoref)*Bref*B_gauss*rhorefStar*rhoref)
@@ -1292,7 +1488,7 @@ def RIP_k_space_sum_IDL(suffix,iterdb_file_name,manual_Doppler,\
     
     nky0=len(n_list)
     ntime=len(time_list)
-    B1_ky_t=np.zeros((nky0,ntime))
+    B1_ky_t=np.zeros((nky0,ntime),dtype=complex)
 
     if os.path.isdir(csv_path):  #if path does not exist, then create 'csv'
         pass
@@ -1308,7 +1504,7 @@ def RIP_k_space_sum_IDL(suffix,iterdb_file_name,manual_Doppler,\
         time0=time_list[i]
         itime = np.argmin(abs(time - time0))
         field.set_time(time_list[i])
-        print("Looking at the spectra at time:"+str(time[itime]))
+        #print("Looking at the spectra at time:"+str(time[itime]))
         #This sets the time step you want to read in
         #field.set_time(time[itime])
         
@@ -1339,7 +1535,7 @@ def RIP_k_space_sum_IDL(suffix,iterdb_file_name,manual_Doppler,\
 
 
             B1_GENE_ky0=apar_ky*(ky_GENE_grid*Apar_to_B1)**2.
-            B1_GENE_ky= np.zeros(np.shape(B1_GENE_ky0[0,:]))
+            B1_GENE_ky= np.zeros(np.shape(B1_GENE_ky0[0,:]),dtype=complex)
             
 
             #plt.clf()
